182ec7c6a5b7090611fe85d02ad5e4c57a70920e
yxp <dm2267747196@qq.com>
2020-04-28 14:39:59 +0800

    van-tab 测试

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+<div class="container">
+  <div class="plus" @click="show=!show">+</div>
+  <div class="top" style="background-color: #42B983;height:40px;">
+    <ul>
+      <li class="title" :class="{'on':active==0}" @click="selectActive(0)">管理</li>
+      <li class="title" :class="{'on':active==1}" @click="selectActive(1)">化验</li>
+      <li class="title" :class="{'on':active==2}" @click="selectActive(2)">巡检</li>
+      <li class="title" :class="{'on':active==3}" @click="selectActive(3)">养护</li>
+      <li class="title" :class="{'on':active==4}" @click="selectActive(4)">维修</li>
+      <li class="title" :class="{'on':active==5}" @click="selectActive(5)">保养</li>
+    </ul>
+
+      <van-action-sheet v-model="show" :actions="plusList" />
+  </div>
+</div>
+<div class="toggle">
+<one v-if="active==1"></one>
+<two v-if="active==2"></two>
+<three v-if="active==3"></three>
+<four v-if="active==4"></four>
+<five v-if="active==5"></five>
+</div>
+  </div>
+</template>
+
+<script>
+  import one from './one.vue'
+  import two from './two.vue'
+  import three from './three.vue'
+  import four from './four.vue'
+  import five from './five.vue'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      show:false,
+      active:0,
+      plusList:[{id:0,name:"报修"},{id:1,name:"维护"}]
+    }
+  },
+  components:{
+    one,two,three,four,five
+  },
+  methods:{
+    selectActive(e){
+      this.active=e
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/one.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="hello1">
+111111
+<van-radio-group v-model="radio">
+  <van-radio name="1">
+    <van-field  v-model="timerName" placeholder="请选择间隔时间" @click="choose=true" readonly></van-field>
+  </van-radio>
+  <van-radio name="2">
+    <van-field label="开始时间" readonly @click="choose1=true">
+
+    </van-field>
+    <van-field label="结束时间" readonly >
+    </van-field>
+  </van-radio>
+</van-radio-group>
+<van-action-sheet v-model="choose" :actions="arr" @select="onSelect" ></van-action-sheet>
+<van-popup v-model="choose1" position="bottom">
+   <van-datetime-picker v-model="currentDate" :min-date="minDate" :max-date="maxDate" @cancel="cancel"></van-datetime-picker>
+</van-popup>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      show:false,
+      radio:"1",
+      timerValue:'',
+      timerName:'',
+      choose:false,
+      arr:[{id:0,name:"1小时"},{id:1,name:"2小时"},{id:2,name:"3小时"}],
+
+      choose1:false,
+
+      minDate:new Date(2000,1,1),
+      maxDate:new Date(2025,1,1),
+      currentDate:new Date()
+    }
+  },
+  methods:{
+    onSelect(i){
+      console.log(i)
+      this.choose=false
+      this.timerValue=i.id
+      this.timerName=i.name
+      console.log(2)
+    },
+    cancel(){
+      console.log("取消")
+      this.choose1=false
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
